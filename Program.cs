23497a0ec41af87c1284c046de622e451375f392
Duvian <darlycustom@gmail.com>
2023-09-15 20:45:19 -0500

    este es mi primer get hecho de la primer manera :D

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
 ﻿using System;
+       
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Estudios respuesta = new Estudios("ingenieria mecatronica");
+        Console.WriteLine(respuesta.nombreCarrera());
+    }
+}
 
-class Program(){
-    public static void Main(string[] args){
 
-        Console.WriteLine("Hola mundo");
-    
+class Estudios
+{   //atributos
+    private string _nombreCarrera;
+    //cosntructor
+    public Estudios (string nombreCarrera)
+    {
+        this._nombreCarrera = nombreCarrera;
+    }
+
+    //metodo
+    public string nombreCarrera()
+    {
+        return this._nombreCarrera;
     }
 }
