c275c66bcafb499f446204a70c2cefe8999180fa
Hernan <hernaneltriller@gmail.com>
2023-09-15 21:03:53 -0500

    cree una clase

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program{
 
     public static void Main(string[] args){
 
-        //
+
+        Vehiculo mazda = new Vehiculo();
+
+        Console.WriteLine(mazda.carro());
     }
 }
---
+++ class/class.cs
@@ -0,0 +1,14 @@
+class Vehiculo{
+
+    private string _carro;
+
+    private string _moto;
+
+    public Vehiculo(string carro){
+        this._carro = carro;
+    }
+
+    public string carro(){
+        return this.carro;
+    }
+}
