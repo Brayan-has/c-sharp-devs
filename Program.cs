4f84cfd6a43dc9b6c4abb34917a9600045047c89
Brayan-has <g.nayarb112@gmail.com>
2023-09-16 20:40:02 -0600

    tarea para el lunes: 18 de septiembre 2023

--- Program.cs
+++ Program.cs
@@ -2,8 +2,13 @@
 
 class Program{
 
-    public static void Main(string[] args){
+  public static void Main(string[] args){
+  
+  /*crear una clase de tipo pélicula que tenga 4 atributos, cada atributo debe tener un método get y un método set, se deberá poner un valor por defecto para cada atributo en el constructor, (sin pedirse lo al usuario/desarrollador).
 
-        //primera tarea del día:
-    }
+  Se debe instanciar 4 objetos diferentes y reasignar el valor de cada atributo por el que usted quiera, solo va a dejar los valores por defecto del primer objeto. Alfinal almacenar cada objeto (instancia) en un arreglo.
+  */ 
+    
+  }
 }
+
