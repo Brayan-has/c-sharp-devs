3d9ed3b84614b1b1cc3f159c257cb84eb29e7086
Brayan-has <g.nayarb112@gmail.com>
2023-09-16 20:44:35 -0600

    última modificación

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ class Program{
   
   /*crear una clase de tipo pélicula que tenga 4 atributos, cada atributo debe tener un método get y un método set, se deberá poner un valor por defecto para cada atributo en el constructor, (sin pedirse lo al usuario/desarrollador).
 
-  Se debe instanciar 4 objetos diferentes y reasignar el valor de cada atributo por el que usted quiera, solo va a dejar los valores por defecto del primer objeto. Alfinal almacenar cada objeto (instancia) en un arreglo.
+  Se debe instanciar 4 objetos diferentes y reasignar el valor de cada atributo por el que usted quiera, solo va a dejar los valores por defecto del primer objeto. Alfinal almacenar cada objeto (instancia) en un arreglo
+  
+  pdata: La clase debe estar en otro archivo .cs en otra carpeta.
   */ 
     
   }
