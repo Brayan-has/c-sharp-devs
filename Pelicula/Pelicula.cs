95924f3752d129ba843eaeb20ee91d91fb0381bf
Brayan-has <g.nayarb112@gmail.com>
2023-09-25 19:20:43 -0600

    agrego la clase con sus componentes básicos

---
+++ Pelicula/Pelicula.cs
@@ -0,0 +1,21 @@
+
+//Primero creo la clase Película
+class Pelicula{
+
+    //Aquí irán los atributos
+    private string nombre;
+    private string duracion;
+    private string critica;
+    private string puntajeFinal;
+    //Aquí irá el constructor
+    public Pelicula(){
+
+        nombre = "";
+        duracion = "";
+        critica = "";
+        puntajeFinal = "";
+
+    } 
+    //Aquí irán los métodos
+    
+}
